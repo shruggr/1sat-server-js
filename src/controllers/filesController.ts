c0caff3eef86e26fa70637bea0f5a9449d75f59c
David Case <david.case@shruggr.cloud>
2023-03-26 13:15:36 -0400

    Fix files API

--- src/controllers/filesController.ts
+++ src/controllers/filesController.ts
@@ -2,7 +2,7 @@ import {JungleBusClient} from "@gorillapool/js-junglebus";
 import { Tx } from "@ts-bitcoin/core";
 import { Request as ExpRequest } from "express";
 import { NotFound } from 'http-errors';
-import { Readable } from 'stream';
+// import { Readable } from 'stream';
 import { Controller, Get, Path, Request, Route } from "tsoa";
 import { Inscription, Origin } from "../models";
 
@@ -22,18 +22,9 @@ export class FilesController extends Controller {
         const insData = Inscription.parseOutputScript(tx.txOuts[im.vout].script);
 
         if(!insData.data) throw new NotFound('not-found');
-        if(insData.type) this.setHeader('content-type', insData.type || '');
-        this.setStatus(200);
-        this.setHeader('cache-control', 'public,immutable,max-age=31536000')
-        
         if (!req.res) throw new Error("No response object");
-        const res = req.res;
-        return new Promise(async (resolve, reject) => 
-            Readable.from(insData.data || Buffer.alloc(0))
-                .on('error', reject)
-                .on('end', function () {
-                    resolve(undefined)
-                })
-                .pipe(res));
+        req.res.header('Content-Type', insData.type || '');
+        req.res.header('Cache-Control', 'public,immutable,max-age=31536000')
+        req.res.status(200).send(insData.data);
     }
 }
