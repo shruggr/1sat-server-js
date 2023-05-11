8975e967823cb46bfdcc28ce3e62796fd9a63534
David Case <david@fyxgaming.com>
2023-05-11 15:31:18 -0400

    Include payout in inventory

--- src/models/txo.ts
+++ src/models/txo.ts
@@ -57,7 +57,7 @@ export class Txo {
 
     static async loadInscriptionsByLock(lock: string, limit = 100, offset = 0): Promise<Inscription[]> {
         const { rows } = await pool.query(`
-            SELECT i.id, t.txid, t.vout, i.filehash, i.filesize, i.filetype, t.origin, t.height, t.idx, t.lock, t.spend, i.map, t.listing, l.price
+            SELECT i.id, t.txid, t.vout, i.filehash, i.filesize, i.filetype, t.origin, t.height, t.idx, t.lock, t.spend, i.map, t.listing, l.price, l.payout
             FROM txos t
             JOIN inscriptions i ON i.origin=t.origin
             LEFT JOIN ordinal_lock_listings l ON l.txid=t.txid AND l.vout=t.vout
