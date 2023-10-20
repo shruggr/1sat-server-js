f93d63ffa66f432f3e0656a73ac2cf65c4df8a3c
David Case <david.case@shruggr.cloud>
2023-10-20 17:29:47 +0000

    Fetch tx bin from JB

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 import { Redis } from "ioredis";
 import { Tx } from '@ts-bitcoin/core';
 import { JungleBusClient } from "@gorillapool/js-junglebus";
-import { NotFound } from 'http-errors';
+import * as createError from 'http-errors';
 
 const { POSTGRES } = process.env;
 export const jb = new JungleBusClient('https://junglebus.gorillapool.io');
@@ -12,11 +12,17 @@ export const pool = new Pool({ connectionString: POSTGRES});
 export async function loadTx(txid: string): Promise<Tx> {
     let rawtx = await redis.getBuffer(txid);
     if (!rawtx) {
-        const jbtx = await jb.GetTransaction(txid);
-        if (!jbtx) {
-            throw new NotFound(`Transaction ${txid} not found`);
+        const resp = await fetch(`https://junglebus.gorillapool.io/v1/transaction/get/${txid}/bin`);
+        if(!resp.ok) {
+            throw createError(resp.status, resp.statusText)
         }
-        rawtx = Buffer.from(jbtx.transaction, 'base64');
+        rawtx = Buffer.from(await resp.arrayBuffer());
+        await redis.set(txid, rawtx)
+        // const jbtx = await jb.GetTransaction(txid);
+        // if (!jbtx) {
+        //     throw new NotFound(`Transaction ${txid} not found`);
+        // }
+        // rawtx = Buffer.from(jbtx.transaction, 'base64');
     }
     return Tx.fromBuffer(rawtx);
 }
