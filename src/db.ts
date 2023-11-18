34a9c893a61b5e2eab2e5d7a1eeaf1f3b7f54608
David Case <david.case@shruggr.cloud>
2023-11-18 23:27:23 +0000

    data

--- src/db.ts
+++ src/db.ts
@@ -1,8 +1,8 @@
-import { Pool } from 'pg';
-import { Redis } from "ioredis";
-import { Tx } from '@ts-bitcoin/core';
 import { JungleBusClient } from "@gorillapool/js-junglebus";
-import * as createError from 'http-errors';
+import { Tx } from '@ts-bitcoin/core';
+import createError, { NotFound } from 'http-errors';
+import { Redis } from "ioredis";
+import { Pool } from 'pg';
 
 const { POSTGRES_FULL, BITCOIN_HOST, BITCOIN_PORT } = process.env;
 export const jb = new JungleBusClient('https://junglebus.gorillapool.io');
@@ -13,18 +13,34 @@ export const pool = new Pool({ connectionString: POSTGRES_FULL});
 export async function loadTx(txid: string): Promise<Tx> {
     let rawtx = await redis.getBuffer(txid);
     if (!rawtx) {
-        const url = `http://${BITCOIN_HOST}:${BITCOIN_PORT}/rest/tx/${txid}.bin`
-        const resp = await fetch(url);
-        if(!resp.ok) {
-            throw createError(resp.status, resp.statusText)
+        try {
+            const url = `http://junglebus.gorillapool.io/v1/transaction/get/${txid}.bin`
+            const resp = await fetch(url);
+            if(!resp.ok) {
+                throw createError(resp.status, resp.statusText)
+            }
+            rawtx = Buffer.from(await resp.arrayBuffer());
+            await redis.set(txid, rawtx)
+        } catch {
+            console.log('Fetch from JB error:', txid)
         }
-        rawtx = Buffer.from(await resp.arrayBuffer());
-        await redis.set(txid, rawtx)
-        // const jbtx = await jb.GetTransaction(txid);
-        // if (!jbtx) {
-        //     throw new NotFound(`Transaction ${txid} not found`);
-        // }
-        // rawtx = Buffer.from(jbtx.transaction, 'base64');
+    }
+    if (!rawtx) {
+        try {
+            const url = `http://${BITCOIN_HOST}:${BITCOIN_PORT}/rest/tx/${txid}.bin`
+            const resp = await fetch(url);
+            if(!resp.ok) {
+                throw createError(resp.status, resp.statusText)
+            }
+            rawtx = Buffer.from(await resp.arrayBuffer());
+            await redis.set(txid, rawtx)
+        } catch {
+            console.log('Fetch from server error:', txid)
+        }
+    }
+
+    if (!rawtx) {
+        throw new NotFound(`Transaction ${txid} not found`);
     }
     return Tx.fromBuffer(rawtx);
 }
