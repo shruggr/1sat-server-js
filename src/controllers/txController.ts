26ef0bee3d09b56fbd50647d5b66623a00c5e471
David Case <david.case@shruggr.cloud>
2023-12-03 17:27:13 +0000

    feat: add WoC broadcast for testnet

--- src/controllers/txController.ts
+++ src/controllers/txController.ts
@@ -3,7 +3,7 @@ import { Redis } from "ioredis";
 import { Body, BodyProp, Controller, Get, Path, Post, Route } from "tsoa";
 import { Tx } from "@ts-bitcoin/core";
 
-const { ARC, ARC_TOKEN, TAAL_TOKEN } = process.env;
+const { ARC, ARC_TOKEN, NETWORK, TAAL_TOKEN } = process.env;
 const pubClient = new Redis();
 export interface PreviousOutput {
     lockingScript: string,
@@ -24,7 +24,7 @@ export class TxController extends Controller {
         @BodyProp() rawtx: string,
     ): Promise<string> {
         const txbuf = Buffer.from(rawtx, 'base64')
-        
+
         return this.doBroadcast(txbuf);
     }
 
@@ -34,12 +34,22 @@ export class TxController extends Controller {
         console.log('Broadcasting TX:', txid, tx.toHex());
 
         try {
-            if(TAAL_TOKEN) {
+            if (NETWORK == 'testnet') {
+                try {
+                    await this.broadcastWOC(txbuf);
+                    this.broadcastArc(txbuf).catch(console.error)
+                } catch (e: any) {
+                    if (e.status && e.status >= 300 && e.status < 500) {
+                        throw e;
+                    }
+                    await this.broadcastArc(txbuf);
+                }
+            } else if (TAAL_TOKEN) {
                 try {
                     await this.broadcastTaal(txbuf);
                     this.broadcastArc(txbuf).catch(console.error)
-                } catch(e: any) {
-                    if(e.status && e.status >= 300 && e.status < 500) {
+                } catch (e: any) {
+                    if (e.status && e.status >= 300 && e.status < 500) {
                         throw e;
                     }
                     await this.broadcastArc(txbuf);
@@ -47,14 +57,38 @@ export class TxController extends Controller {
             } else {
                 await this.broadcastArc(txbuf);
             }
-           
+
             pubClient.publish('submit', txid);
             return txid;
-        } catch(e: any) {
+        } catch (e: any) {
             console.error("Broadcast Error:", e)
             throw e;
         }
 
+    }
+
+    async broadcastWOC(txbuf: Buffer) {
+        const net = NETWORK == 'testnet' ? 'test' : 'main'
+        const resp = await fetch(`https://api.whatsonchain.com/v1/bsv/${net}/tx/raw`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({txhex: txbuf.toString('hex')})
+        });
+
+        const respText = await resp.text();
+        console.log("WOC Response:", resp.status, respText);
+        if (!resp.ok) {
+            try {
+                // const { status, error } = JSON.parse(respText);
+                // if (!error.includes('txn-already-known')) {
+                    throw createError(resp.status || 500, `Broadcast failed: ${respText}}`);
+                // }
+            } catch (e: any) {
+                throw e;
+            }
+        }
     }
 
     async broadcastTaal(txbuf: Buffer) {
@@ -72,21 +106,21 @@ export class TxController extends Controller {
         if (!resp.ok) {
             try {
                 const { status, error } = JSON.parse(respText);
-                if(!error.includes('txn-already-known')) {
+                if (!error.includes('txn-already-known')) {
                     throw createError(status || resp.status || 500, `Broadcast failed: ${error}`);
                 }
-            } catch(e: any) {
+            } catch (e: any) {
                 throw e;
             }
         }
     }
 
     async broadcastArc(txbuf: Buffer) {
-        const headers: {[key:string]:string} = {
+        const headers: { [key: string]: string } = {
             'Content-Type': 'application/octet-stream',
             'X-WaitForStatus': '7'
         }
-        if(ARC_TOKEN) {
+        if (ARC_TOKEN) {
             headers['Authorization'] = `Bearer ${ARC_TOKEN}`
         }
         const resp = await fetch(`${ARC}/v1/tx`, {
@@ -96,13 +130,9 @@ export class TxController extends Controller {
         })
         const respText = await resp.text();
         console.log("ARC Response:", resp.status, respText);
-        try {
-            const result = JSON.parse(respText)
-            if (result.status != 200) {
-                throw createError(result.status || 500, `Broadcast failed: ${result.detail} ${result.extraInfo}`);
-            }
-        } catch {
-            throw createError(500, `Broadcast failed: parsing error`);
+        const result = JSON.parse(respText)
+        if (result.status != 200 || result.txStatus == 'REJECTED') {
+            throw createError(result.status || 500, `Broadcast failed: ${result.detail} ${result.extraInfo}`);
         }
     }
 
